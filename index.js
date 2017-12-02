77c096f50226b17421f40faa71b6e38d7bcc7369
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-12-02 11:51:07 -0800

    Add tests for non-scalar non-object values and also correctly handle empty objects

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import reduceWithValueKey from "@unction/reducewithvaluekey"
 import reduceValues from "@unction/reducevalues"
+import isPopulated from "@unction/ispopulated"
 import isType from "@unction/istype"
 import mapValues from "@unction/mapvalues"
 import prepend from "@unction/prepend"
@@ -7,10 +8,10 @@ import append from "@unction/append"
 import mergeRight from "@unction/mergeright"
 import {join} from "ramda"
 
-export default function flattenTree (delimiter: StringType): Function {
+export default function flattenTree (delimiter: StringType): UnaryFunctionType {
   return function flattenTreeDelimiter (recordTree: RecordTreeType): RecordType {
     return reduceValues(
-      (accumulated: RecordType): Function =>
+      (accumulated: RecordType): UnaryFunctionType =>
         ([keys, value]: Array<ValueType>): RecordType =>
           mergeRight(
             accumulated
@@ -23,10 +24,10 @@ export default function flattenTree (delimiter: StringType): Function {
       (
         function flattenTreeDelimiterMapping (nested: RecordType): RecordType {
           return reduceWithValueKey(
-            (accumulated: RecordType): Function =>
-              (treeOrLeaf: ValueType): Function =>
+            (accumulated: RecordType): UnaryFunctionType =>
+              (treeOrLeaf: ValueType): UnaryFunctionType =>
                 (key: KeyType): RecordType => {
-                  if (isType("Object")(treeOrLeaf)) {
+                  if (isType("Object")(treeOrLeaf) && isPopulated(treeOrLeaf)) {
                     return mergeRight(
                       accumulated
                     )(
