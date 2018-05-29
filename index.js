eab66a673800325b7d8bd22d940c0f7fe9b2ab7f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-28 17:34:26 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,56 +1,24 @@
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import reduceValues from "@unction/reducevalues"
-import isPopulated from "@unction/ispopulated"
-import isType from "@unction/istype"
-import mapValues from "@unction/mapvalues"
-import prepend from "@unction/prepend"
-import append from "@unction/append"
-import mergeRight from "@unction/mergeright"
-import {join} from "ramda"
-
-export default function flattenTree (delimiter: StringType): UnaryFunctionType {
-  return function flattenTreeDelimiter (recordTree: RecordTreeType): RecordType {
-    return reduceValues(
-      (accumulated: RecordType): UnaryFunctionType =>
-        ([keys, value]: Array<ValueType>): RecordType =>
-          mergeRight(
-            accumulated
-          )(
-            {[join(delimiter)(keys)]: value}
-          )
-    )(
-      {}
-    )(
-      (
-        function flattenTreeDelimiterMapping (nested: RecordType): RecordType {
-          return reduceWithValueKey(
-            (accumulated: RecordType): UnaryFunctionType =>
-              (treeOrLeaf: ValueType): UnaryFunctionType =>
-                (key: KeyType): RecordType => {
-                  if (isType("Object")(treeOrLeaf) && isPopulated(treeOrLeaf)) {
-                    return mergeRight(
-                      accumulated
-                    )(
-                      mapValues(
-                        ([keys, leaf]: [Array<KeyType>, ValueType]): [Array<KeyType>, ValueType] =>
-                          append(leaf)([prepend(key)(keys)])
-                      )(
-                        flattenTreeDelimiterMapping(treeOrLeaf)
-                      )
-                    )
-                  }
-
-                  return prepend([[key], treeOrLeaf])(accumulated)
-                }
-          )(
-            []
-          )(
-            nested
-          )
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import reduceValues from "@unction/reducevalues";
+import isPopulated from "@unction/ispopulated";
+import isType from "@unction/istype";
+import mapValues from "@unction/mapvalues";
+import prepend from "@unction/prepend";
+import append from "@unction/append";
+import mergeRight from "@unction/mergeright";
+import { join } from "ramda";
+export default function flattenTree(delimiter) {
+  return function flattenTreeDelimiter(recordTree) {
+    return reduceValues(accumulated => ([keys, value]) => mergeRight(accumulated)({
+      [join(delimiter)(keys)]: value
+    }))({})(function flattenTreeDelimiterMapping(nested) {
+      return reduceWithValueKey(accumulated => treeOrLeaf => key => {
+        if (isType("Object")(treeOrLeaf) && isPopulated(treeOrLeaf)) {
+          return mergeRight(accumulated)(mapValues(([keys, leaf]) => append(leaf)([prepend(key)(keys)]))(flattenTreeDelimiterMapping(treeOrLeaf)));
         }
-      )(
-        recordTree
-      )
-    )
-  }
+
+        return prepend([[key], treeOrLeaf])(accumulated);
+      })([])(nested);
+    }(recordTree));
+  };
 }
