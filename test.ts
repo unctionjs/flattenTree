84118ce2113fda660c4688e98020d9a20b840189
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:45 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-magic-numbers */
+import flattenTree from "./index.ts";
+
+test(() => {
+  expect(flattenTree("-")(
+    {
+      aria: {role: "navigation"},
+      data: {
+        profile: {
+          name: "Kurtis Rainbolt-Greene",
+          age: 24,
+        },
+        metadata: {interval: "10s"},
+        location: "http://api.example.com/profiles/24",
+        payload: {},
+        points: [1, 2, 3],
+      },
+    }
+  )).toEqual({
+    "data-profile-name": "Kurtis Rainbolt-Greene",
+    "data-profile-age": 24,
+    "data-metadata-interval": "10s",
+    "data-location": "http://api.example.com/profiles/24",
+    "data-payload": {},
+    "data-points": [1, 2, 3],
+    "aria-role": "navigation",
+  });
+});
